62a048f7a5255126f35aa741af7bfc0960a9b01f
bogdan <you@example.com>
2023-11-08 16:53:45 +0200

    multi module maven

--- PetClinicWeb/src/main/java/ClinicExample/PetClinic/PetClinicApplication.java
+++ PetClinicWeb/src/main/java/ClinicExample/PetClinic/PetClinicApplication.java
@@ -8,6 +8,16 @@ public class PetClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
+
+		System.out.println("test");
+
+
+
+
 	}
+
+
+
+
 
 }
